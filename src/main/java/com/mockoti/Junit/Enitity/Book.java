1b65e0b2a5b4f9e18f94b2a96e90fd07d1105a2f
giridharan.ms_werize <giridharan.ms@werize.com>
2024-10-13 20:32:26 +0530

    added no args and all args constructor Book class

--- src/main/java/com/mockoti/Junit/Enitity/Book.java
+++ src/main/java/com/mockoti/Junit/Enitity/Book.java
@@ -1,13 +1,17 @@
 package com.mockoti.Junit.Enitity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Book")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
